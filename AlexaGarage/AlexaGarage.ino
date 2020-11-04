730108a14e0732bae9b19d4b0b861f9a60c6f0ed
Bruno F Valli <brunofvalli@gmail.com>
2020-11-04 13:26:33 -0600

    No compiling due to issues on library

--- AlexaGarage/AlexaGarage.ino
+++ AlexaGarage/AlexaGarage.ino
@@ -1,143 +1,84 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <WiFiUdp.h>
-#include <functional>
-#include "switch.h"
-#include "UpnpBroadcastResponder.h"
-#include "CallbackFunction.h"
+#include <Arduino.h>
+#ifdef ESP32
+  #include <WiFi.h>
+  #define RELAY_PIN_1 12
+  #define RELAY_PIN_2 14
+#else
+  #include <ESP8266WiFi.h>
+  //#define RF_RECEIVER 5
+  #define RELAY_PIN_1 4
+  #define RELAY_PIN_2 14
+#endif
+#include "fauxmoESP.h"
+#define SERIAL_BAUDRATE 115200
 
-#define GARAGE_ONE_RELAY_PIN 5
-#define GARAGE_TWO_RELAY_PIN 6
+#define WIFI_SSID "5124EscambiaTerr"
+#define WIFI_PASS "M4v1e?Brun0"
+#define app1 "bedroom light"
+#define app2 "tv"
+fauxmoESP fauxmo;
 
-// prototypes
-boolean connectWifi();
-
-//on/off callbacks 
-bool garageOneOpen();
-bool garageOneClose();
-bool garageTwoOpen();
-bool garageTwoClose();
-
-// Change this before you flash
-const char* ssid = "5124EscambiaTerr";
-const char* password = "M4v1e?Brun0";
-
-boolean wifiConnected = false;
-
-UpnpBroadcastResponder upnpBroadcastResponder;
-
-Switch *garageOne = NULL;
-Switch *garageTwo = NULL;
-
-bool isGarageOneOpen = false;
-bool isGarageTwoOpen = false;
-
-void setup()
-{
-  Serial.begin(9600);
-   
-  // Initialise wifi connection
-  wifiConnected = connectWifi();
-  
-  if(wifiConnected){
-    upnpBroadcastResponder.beginUdpMulticast();
-    
-    // Define your switches here. Max 10
-    // Format: Alexa invocation name, local port no, on callback, off callback
-    garageOne = new Switch("GarageOne", 80, garageOneOpen, garageOneClose);
-    garageTwo = new Switch("GarageTwo", 81, garageTwoOpen, garageTwoClose);
-
-    Serial.println("Adding switches upnp broadcast responder");
-    upnpBroadcastResponder.addDevice(*garageOne);
-    upnpBroadcastResponder.addDevice(*garageTwo);
-  }
-}
- 
-void loop()
-{
-	 if(wifiConnected){
-      upnpBroadcastResponder.serverLoop();
-      
-      garageTwo->serverLoop();
-      garageOne->serverLoop();
-	 }
-}
-
-bool pressButton(int relayPin)
-{
-  digitalWrite(relayPin, HIGH);
-  digitalWrite(relayPin, LOW);
-}
-
-bool garageOneOpen() {
-    Serial.println("Opening garage one ...");
-
-    pressButton(GARAGE_ONE_RELAY_PIN);
-    
-    isGarageOneOpen = true;    
-    return isGarageOneOpen;
-}
-
-bool garageOneClose() {
-    Serial.println("Closing garage one ...");
-
-    pressButton(GARAGE_ONE_RELAY_PIN);
-
-    isGarageOneOpen = false;
-    return isGarageOneOpen;
-}
-
-bool garageTwoOpen() {
-    Serial.println("Opening garage 2 ...");
-
-    pressButton(GARAGE_TWO_RELAY_PIN);
-
-    isGarageTwoOpen = true;
-    return isGarageTwoOpen;
-}
-
-bool garageTwoClose() {
-  Serial.println("Closing garage 2 ...");
-
-  pressButton(GARAGE_TWO_RELAY_PIN);
-
-  isGarageTwoOpen = false;
-  return isGarageTwoOpen;
-}
-
-// connect to wifi – returns true if successful or false if not
-boolean connectWifi(){
-  boolean state = true;
-  int i = 0;
-  
+void wifiSetup() {
+  // Set WIFI module to STA mode
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-  Serial.println("Connecting to WiFi");
-
-  // Wait for connection
-  Serial.print("Connecting ...");
+  Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
     Serial.print(".");
-    if (i > 10){
-      state = false;
-      break;
+    delay(100);
+  }
+  Serial.println();
+  Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+}
+
+void setup() {
+  // Init serial port and clean garbage
+  Serial.begin(SERIAL_BAUDRATE);
+  Serial.println();
+
+  // Wi-Fi connection
+  wifiSetup();
+
+  pinMode(RELAY_PIN_1, OUTPUT);
+  digitalWrite(RELAY_PIN_1, HIGH);
+
+  pinMode(RELAY_PIN_2, OUTPUT);
+  digitalWrite(RELAY_PIN_2, HIGH);
+  
+  fauxmo.createServer(true); 
+  fauxmo.setPort(80); 
+  fauxmo.enable(true);
+  fauxmo.addDevice(app1);
+  fauxmo.addDevice(app2);
+
+  fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {    
+    Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+    if ( (strcmp(device_name, app1) == 0) ) {
+      Serial.println("RELAY 1 switched by Alexa");
+      if (state) {
+        digitalWrite(RELAY_PIN_1, LOW);
+      } else {
+        digitalWrite(RELAY_PIN_1, HIGH);
+      }
     }
-    i++;
+    if ( (strcmp(device_name, app2) == 0) ) {
+      Serial.println("RELAY 2 switched by Alexa");
+      if (state) {
+        digitalWrite(RELAY_PIN_2, LOW);
+      } else {
+        digitalWrite(RELAY_PIN_2, HIGH);
+      }
+    }
+  });
+
+}
+
+void loop() {
+  fauxmo.handle();
+
+  static unsigned long last = millis();
+  if (millis() - last > 5000) {
+    last = millis();
+    Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
   }
-  
-  if (state){
-    Serial.println("");
-    Serial.print("Connected to ");
-    Serial.println(ssid);
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-  }
-  else {
-    Serial.println("");
-    Serial.println("Connection failed.");
-  }
-  
-  return state;
 }
