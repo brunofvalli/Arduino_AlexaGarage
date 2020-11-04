df8e26613d291fa985aeb614e50210fea3aae21c
Bruno F Valli <brunofvalli@gmail.com>
2020-11-04 13:44:25 -0600

    Failure on library useage

--- AlexaGarage/AlexaGarage.ino
+++ AlexaGarage/AlexaGarage.ino
@@ -1,84 +1,62 @@
 #include <Arduino.h>
-#ifdef ESP32
-  #include <WiFi.h>
-  #define RELAY_PIN_1 12
-  #define RELAY_PIN_2 14
-#else
-  #include <ESP8266WiFi.h>
-  //#define RF_RECEIVER 5
-  #define RELAY_PIN_1 4
-  #define RELAY_PIN_2 14
-#endif
+#include <ESP8266WiFi.h>
 #include "fauxmoESP.h"
-#define SERIAL_BAUDRATE 115200
 
-#define WIFI_SSID "5124EscambiaTerr"
-#define WIFI_PASS "M4v1e?Brun0"
-#define app1 "bedroom light"
-#define app2 "tv"
+#define WIFI_SSID "ATT6s3ENT4"
+#define WIFI_PASS "69z5f2s4+e4p"
+#define SERIAL_BAUDRATE                 115200
+
 fauxmoESP fauxmo;
 
+// -----------------------------------------------------------------------------
+// Wifi
+// -----------------------------------------------------------------------------
+
 void wifiSetup() {
-  // Set WIFI module to STA mode
-  WiFi.mode(WIFI_STA);
-  Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(100);
+
+    // Set WIFI module to STA mode
+    WiFi.mode(WIFI_STA);
+
+    // Connect
+    Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    // Wait
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(100);
+    }
+    Serial.println();
+
+    // Connected!
+    Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+}
+
+void callback(uint8_t device_id, const char * device_name, bool state) {
+  Serial.print("Device "); Serial.print(device_name); 
+  Serial.print(" state: ");
+  if (state) {
+    Serial.println("ON");
+  } else {
+    Serial.println("OFF");
   }
-  Serial.println();
-  Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
 }
 
 void setup() {
-  // Init serial port and clean garbage
-  Serial.begin(SERIAL_BAUDRATE);
-  Serial.println();
+    // Init serial port and clean garbage
+    Serial.begin(SERIAL_BAUDRATE);
+    Serial.println("FauxMo demo sketch");
+    Serial.println("After connection, ask Alexa/Echo to 'turn <devicename> on' or 'off'");
 
-  // Wi-Fi connection
-  wifiSetup();
+    // Wifi
+    wifiSetup();
 
-  pinMode(RELAY_PIN_1, OUTPUT);
-  digitalWrite(RELAY_PIN_1, HIGH);
-
-  pinMode(RELAY_PIN_2, OUTPUT);
-  digitalWrite(RELAY_PIN_2, HIGH);
-  
-  fauxmo.createServer(true); 
-  fauxmo.setPort(80); 
-  fauxmo.enable(true);
-  fauxmo.addDevice(app1);
-  fauxmo.addDevice(app2);
-
-  fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {    
-    Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
-    if ( (strcmp(device_name, app1) == 0) ) {
-      Serial.println("RELAY 1 switched by Alexa");
-      if (state) {
-        digitalWrite(RELAY_PIN_1, LOW);
-      } else {
-        digitalWrite(RELAY_PIN_1, HIGH);
-      }
-    }
-    if ( (strcmp(device_name, app2) == 0) ) {
-      Serial.println("RELAY 2 switched by Alexa");
-      if (state) {
-        digitalWrite(RELAY_PIN_2, LOW);
-      } else {
-        digitalWrite(RELAY_PIN_2, HIGH);
-      }
-    }
-  });
-
+    // Fauxmo
+    fauxmo.addDevice("Fish Tank");
+    //fauxmo.addDevice("pixels");
+    fauxmo.begin(callback);
 }
 
 void loop() {
   fauxmo.handle();
-
-  static unsigned long last = millis();
-  if (millis() - last > 5000) {
-    last = millis();
-    Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
-  }
 }
