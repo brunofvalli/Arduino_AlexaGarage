cadb4eaec732749dbf56f1175e02fd7b05375e68
Bruno F Valli <brunofvalli@gmail.com>
2020-11-04 10:02:32 -0600

    Using Espalexa

--- AlexaGarage/AlexaGarage.ino
+++ AlexaGarage/AlexaGarage.ino
@@ -1,96 +1,205 @@
-#include <Arduino.h>
+/**********************************************************************************
+ *  TITLE: Alexa control 5 channel Relay Module using NodeMCU or ESP32
+ *  Click on the following links to learn more. 
+ *  YouTube Video: https://youtu.be/tIFEtHVLexw
+ *  Related Blog : https://easyelectronicsproject.com/esp32-projects/
+ *  by Subhajit (Tech StudyCell)
+ *  
+ *  Download the libraries:
+ *  https://github.com/Aircoookie/Espalexa
+ *  
+ *  Preferences--> Aditional boards Manager URLs : 
+ *  https://dl.espressif.com/dl/package_esp32_index.json, http://arduino.esp8266.com/stable/package_esp8266com_index.json
+ *  
+ *  Download Board:
+ *  For ESP8266 NodeMCU (2.5.1): https://github.com/esp8266/Arduino
+ *  For ESP32                  : https://github.com/espressif/arduino-esp32
+ **********************************************************************************/
+
+#ifdef ARDUINO_ARCH_ESP32
+#include <WiFi.h>
+#else
 #include <ESP8266WiFi.h>
-#include "fauxmoESP.h"
-#include "ESPAsyncWebServer.h"
-#include <ESPAsyncTCP.h>
-#include <Hash.h>
+#endif
+#include <Espalexa.h>
+// define the GPIO connected with Relays
+#define RelayPin1 5  //D1
+#define RelayPin2 4  //D2
+#define RelayPin3 13 //D7
+#define RelayPin4 14 //D5
+#define RelayPin5 12 //D6
 
-#define WIFI_SSID "" // Please Enter you Wifi name here
-#define WIFI_PASS "" // Enter password here
-#define SERIAL_BAUDRATE                 115200
+// prototypes
+boolean connectWifi();
 
-fauxmoESP fauxmo;
-#define RELAY_PIN 5
-const int  buttonPin = 4;    // the pin that the pushbutton is attached to
-int buttonState = 0;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
+//callback functions
+void firstLightChanged(uint8_t brightness);
+void secondLightChanged(uint8_t brightness);
+void thirdLightChanged(uint8_t brightness);
+void fourthLightChanged(uint8_t brightness);
+void fifthLightChanged(uint8_t brightness);
 
-// -----------------------------------------------------------------------------
-// Wifi
-// -----------------------------------------------------------------------------
+// WiFi Credentials
+const char* ssid = "5124EscambiaTerr";
+const char* password = "M4v1e?Brun0";
 
-void wifiSetup() {
+// device names
+String Device_1_Name = "Garage1";
+String Device_2_Name = "Garage2";
+String Device_3_Name = "Open1";
+String Device_4_Name = "Open2";
+String Device_5_Name = "Open3";
 
-    // Set WIFI module to STA mode
-    WiFi.mode(WIFI_STA);
+boolean wifiConnected = false;
 
-    // Connect
-    Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
-    WiFi.begin(WIFI_SSID, WIFI_PASS);
+Espalexa espalexa;
 
-    // Wait
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(100);
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(RelayPin1, OUTPUT);
+  pinMode(RelayPin2, OUTPUT);
+  pinMode(RelayPin3, OUTPUT);
+  pinMode(RelayPin4, OUTPUT);
+  pinMode(RelayPin5, OUTPUT);
+
+  // Initialise wifi connection
+  wifiConnected = connectWifi();
+
+  if (wifiConnected)
+  {
+    // Define your devices here.
+    espalexa.addDevice(Device_1_Name, firstLightChanged); //simplest definition, default state off
+    espalexa.addDevice(Device_2_Name, secondLightChanged);
+    espalexa.addDevice(Device_3_Name, thirdLightChanged);
+    espalexa.addDevice(Device_4_Name, fourthLightChanged);
+    espalexa.addDevice(Device_5_Name, fifthLightChanged);
+
+    espalexa.begin();
+  }
+  else
+  {
+    while (1)
+    {
+      Serial.println("Cannot connect to WiFi. Please check data and reset the ESP.");
+      delay(2500);
     }
-    Serial.println();
-
-    // Connected!
-    Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+  }
 }
 
-void callback(uint8_t device_id, const char * device_name, bool state) {
-  Serial.print("Device "); Serial.print(device_name); 
-  Serial.print(" state: ");
+void loop()
+{
+  espalexa.loop();
+  delay(1);
+}
+
+//our callback functions
+void firstLightChanged(uint8_t brightness)
+{
+  //Control the device
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin1, HIGH);
+      Serial.println("Device1 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin1, LOW);
+    Serial.println("Device1 OFF");
+  }
+}
+
+void secondLightChanged(uint8_t brightness)
+{
+  //Control the device 
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin2, HIGH);
+      Serial.println("Device2 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin2, LOW);
+    Serial.println("Device2 OFF");
+  }
+}
+
+void thirdLightChanged(uint8_t brightness)
+{
+  //Control the device  
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin3, HIGH);
+      Serial.println("Device3 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin3, LOW);
+    Serial.println("Device3 OFF");
+  }
+}
+
+void fourthLightChanged(uint8_t brightness)
+{
+  //Control the device 
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin4, HIGH);
+      Serial.println("Device4 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin4, LOW);
+    Serial.println("Device4 OFF");
+  }
+}
+
+void fifthLightChanged(uint8_t brightness)
+{
+  //Control the device 
+  if (brightness == 255)
+    {
+      digitalWrite(RelayPin5, HIGH);
+      Serial.println("Device5 ON");
+    }
+  else
+  {
+    digitalWrite(RelayPin5, LOW);
+    Serial.println("Device5 OFF");
+  }
+}
+
+// connect to wifi  returns true if successful or false if not
+boolean connectWifi()
+{
+  boolean state = true;
+  int i = 0;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20) {
+      state = false; break;
+    }
+    i++;
+  }
+  Serial.println("");
   if (state) {
-    Serial.println("ON");
-    digitalWrite(RELAY_PIN, HIGH);
-  } else {
-    Serial.println("OFF");
-    digitalWrite(RELAY_PIN, LOW);
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
   }
-}
-
-void setup() {
-    pinMode(RELAY_PIN, OUTPUT);
-    pinMode(buttonPin, INPUT_PULLUP);
-    digitalWrite(RELAY_PIN, LOW);
-    // Init serial port and clean garbage
-    Serial.begin(SERIAL_BAUDRATE);
-    Serial.println("FauxMo demo sketch");
-    Serial.println("After connection, ask Alexa/Echo to 'turn <devicename> on' or 'off'");
-
-    // Wifi
-    wifiSetup();
-
-    // Fauxmo
-    fauxmo.addDevice("the light");
-    fauxmo.onMessage(callback);
-}
-
-void loop() {
-  fauxmo.handle();
-  
-  // read the pushbutton input pin:
-  buttonState = digitalRead(buttonPin);
-
-  // compare the buttonState to its previous state
-  if (buttonState != lastButtonState) {
-    // if the state has changed, increment the counter
-    if (buttonState == LOW) {
-      Serial.println("on");
-      digitalWrite(RELAY_PIN, HIGH);
-    }
-    else {
-      // if the current state is LOW then the button
-      // went from on to off:
-      Serial.println("off");
-      digitalWrite(RELAY_PIN, LOW);
-    }
-    // Delay a little bit to avoid bouncing
-    delay(50);
+  else {
+    Serial.println("Connection failed.");
   }
-  // save the current state as the last state,
-  //for next time through the loop
-  lastButtonState = buttonState;
+  return state;
 }
